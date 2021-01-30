2c1ebdfa050e3fbe771129e12535216004ac5607
omniamohamed199 <omnia.moha181@gmail.com>
2021-01-30 22:23:32 +0200

    ff

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,26 @@
-console.log('hello')
+const humbergurElm=document.querySelector('#humbergermenue');
+const headerElm=document.querySelector('.header');
+const OverlayElm=document.querySelector('.overlay');
+const fadeElems=document.querySelectorAll('.has-fade');
+const body=document.querySelector('body');
+humbergurElm.addEventListener('click',function()
+{
+    if(headerElm.classList.contains('open'))
+    {//close menue
+        headerElm.classList.remove('open')
+        body.classList.remove('noscroll')
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-in')
+            element.classList.add('fade-out')
+        })
+    }
+    else{//Open menue
+        headerElm.classList.add('open')
+        body.classList.add('noscroll')
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-out')
+            element.classList.add('fade-in')
+        })
+        
+    }
+})
